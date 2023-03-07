3ee9955932c1d8fc9f4499333448d10b53668b5d
icynightcat <khana49@mymacewan.ca>
2023-03-07 03:03:34 -0700

    working demo!!!!

--- settingText.py
+++ settingText.py
@@ -10,6 +10,7 @@ from cohorts import *
 from request_rooms import *
 import scheduleGUI
 
+
 # dictionary to hold cohorts
 program_names = ["PM1", "PM2", "PM3", "BA1", "BA2", "BA3", "GLM1", "GLM2", "GLM3", "FS1", "FS2", "FS3", "DXD1", "DXD2", "DXD3", "BK1", "BK2", "BK3", "PCOM1", "PCOM2", "PCOM3", "BCOM1", "BCOM2", "BCOM3"] #used for naming
 cohorts = {} #store Cohort objects
@@ -28,7 +29,7 @@ def make_cohort(list12):
             cohorts[name] = Cohort(name, num, cohort_num)
             cohort_num += 1
         program_num += 1
-    
+
 
 if __name__ == "__main__":
     #first schedule builder window
@@ -42,27 +43,27 @@ if __name__ == "__main__":
 
     #print(cohorts.keys()) #printing out cohorts made
     #print(cohorts.values())
-    
     #making list of cohorts
     cohort_list = []
     for cohort in cohorts.values(): 
         cohort_list.append(cohort)
-    print("cohort list",cohort_list)
+    #print("cohort list",cohort_list)
 
     #list of rooms
-    print ("rooms list", ROOMS)
+    #print ("rooms list", ROOMS)
 
-    r = algorithm(cohort_list, [Computer_Lab, Room_8])
-    print(r)
+    listOfRooms = algorithm(cohort_list, [Computer_Lab, Room_8])
+    #print("list of rooms scheduled", listOfRooms)
 
+    #Room_8.print_schedule()
+    #Computer_Lab.print_schedule()
 
-
-    '''
     #second schedule builder window
     App2 = QApplication(sys.argv)
-    window = scheduleGUI.MainWindow2()
+    window2 = scheduleGUI.MainWindow2(listOfRooms)
     #start the event loop
     App2.exec()
     print("hello")
-    '''
+
+
 
