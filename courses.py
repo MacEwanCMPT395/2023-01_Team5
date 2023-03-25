928e594702570d58c730a4172929f15f529f6104
ishan perhar <iperhar@ualberta.ca>
2023-03-24 19:45:46 -0600

    completed addCourse, removeCourse, and editCourse functions with documentation

--- courses.py
+++ courses.py
@@ -6,136 +6,197 @@ Function Name: addCourse
 Purpose: add course to our list of courses for the appropriate program and term
 
 Parameters: 
-        program: Which program(abbrev.) does course belong to (eg. PCOM(string)) 
+        list: Which program(abbrev.) does course belong to (eg. PCOM(string)) 
+        valid lists: PCOM_TERM_1, PCOM_TERM_2, PCOM_TERM_3, BCOM_TERM_1, BCOM_TERM_2,
+                     BCOM_TERM_3, FS_TERM_1, FS_TERM_2, FS_TERM_3, DXDI_TERM_1, DXDI_TERM_2
+                     DXDI_TERM_3, BK_TERM_1, BK_TERM_2, BK_TERM_3, GL_TERM_1, GL_TERM_2, 
+                     GL_TERM_3, BA_TERM_1, BA_TERM_2, BA_TERM_3, PM_TERM_1, PM_TERM_2
+                     PM_TERM_3. 
 
-        course: What course and number to be added separated by an underscore
-                (Eg. CMSK_0101(String))
-        term: Which term will this course be taken (Eg. 1, 2, or 3(Int))
+        course_name: What course and number to be added separated by an underscore
+                     (Eg. CMSK_0101(String))
+ 
         hours: How many hours will it take to complete the course (Eg. 34 (Int))
+
         lab: is there a lab component for the lab (Eg. yes or no (string)(will be 
              converted into a boolean))
 
 Example:
-        addCourse(PCOM, CMSK_0101, 1, 34, no)
+        addCourse(PCOM_TERM_1, "CMSK_0101", 34, "no")
 
         Result:
             CMSK_0101 (course object will be created)
             if course doesnt exist it will be added to the array of courses for 
             that program and term
 
-            PCOMT1 = [..., ..., "CMSK_0101"]
-            PCOM_TERM_1 = [..., ..., CMSK_0101]
+            before function call:
+                PCOM_TERM_1 = [..., ...]
+            after:
+                PCOM_TERM_1 = [..., ..., CMSK_0101]
 
 
 '''
-
-def addCourse(program, course, term, hours, lab):
-
+def addCourse(list, course_name, hours, lab):
     #initialize variables
-    program = program.upper()
-    course  = course.upper()
-    term = int(term)
+    course  = course_name.upper()
+    hours = int(hours)
     exist = False
     lab = lab.upper()
 
+    #convert lab requirement to boolean
     if lab == "YES":
         lab = True
     else:
         lab = False
     
+    #check if course is in list
+    if course in list:
+        exist = True
+    
+    #if course not in list create the object and addit to list
+    if exist == False:
+        new_course = Course(course, hours, lab)
+        list.append(new_course)
 
-    #check if course already exists
-    if program == "PCOM":
-        if term == 1:
-            if course in PCOMT1:
-                exist = True
-        elif term == 2:
-            if course in PCOMT2:
-                exist = True
-        elif term == 3:
-            if course in PCOMT3:
-                exist = True
-    elif program == "BCOM":
-        if term == 1:
-            if course in BCOMT1:
-                exist = True
-        elif term == 2:
-            if course in BCOMT2:
-                exist = True
-        elif term == 3:
-            if course in BCOMT3:
-                exist = True
-    elif program == "FS":
-        if term == 1:
-            if course in FST1:
-                exist = True
-        elif term == 2:
-            if course in FST2:
-                exist = True
-        elif term == 3:
-            if course in FST3:
-                exist = True
-    elif program == "DXDI":
-        if term == 1:
-            if course in DXDIT1:
-                exist = True
-        elif term == 2:
-            if course in DXDIT2:
-                exist = True
-        elif term == 3:
-            if course in DXDIT3:
-                exist = True
-    elif program == "BK":
-        if term == 1:
-            if course in BKT1:
-                exist = True
-        elif term == 2:
-            if course in BKT2:
-                exist = True
-        elif term == 3:
-            if course in BKT3:
-                exist = True
-    elif program == "GLM":
-        if term == 1:
-            if course in GLT1:
-                exist = True
-        elif term == 2:
-            if course in GLT2:
-                exist = True
-        elif term == 3:
-            if course in GLT3:
-                exist = True
-    elif program == "BA":
-        if term == 1:
-            if course in BAT1:
-                exist = True
-        elif term == 2:
-            if course in BAT2:
-                exist = True
-        elif term == 3:
-            if course in BAT3:
-                exist = True
-    elif program == "PM":
-        if term == 1:
-            if course in PMT1:
-                exist = True
-        elif term == 2:
-            if course in PMT2:
-                exist = True
-        elif term == 3:
-            if course in PMT3:
-                exist = True
 
-    return lab
+'''
+Function Name: removeCourse
 
-    #if it doesnt exist
-        #append to ref list
-        #create course object
-        #append to list of course objects
+Purpose: removes course from our list of courses for the appropriate program and term
 
+Parameters: 
+        list: Which program(abbrev.) does course belong to (eg. PCOM(string)) 
+        valid lists: PCOM_TERM_1, PCOM_TERM_2, PCOM_TERM_3, BCOM_TERM_1, BCOM_TERM_2,
+                     BCOM_TERM_3, FS_TERM_1, FS_TERM_2, FS_TERM_3, DXDI_TERM_1, DXDI_TERM_2
+                     DXDI_TERM_3, BK_TERM_1, BK_TERM_2, BK_TERM_3, GL_TERM_1, GL_TERM_2, 
+                     GL_TERM_3, BA_TERM_1, BA_TERM_2, BA_TERM_3, PM_TERM_1, PM_TERM_2
+                     PM_TERM_3. 
+
+        course_name: What course and number to be removed separated by an underscore
+                (Eg. CMSK_0101(String))
+        
+
+Example:
+        removeCourse(PCOM, "CMSK_0101")
+
+        Result:
+            CMSK_0101 (course object will be created)
+            if course doesnt exist it will be added to the array of courses for 
+            that program and term
+
+            Before function call:
+                PCOM_TERM_1 = [..., ..., CMSK_0101]
+            After:
+               PCOM_TERM_1 = [..., ...]
+
+
+'''    
+def removeCourse(list, course_name):
+    #initialize variables
+    course  = course_name.upper()
+    exist = False
+    i = 0
+    index = 0
+    
+    #check if course exists and grab its index
+    for i in range(len(list)):
+        if list[i].name == course:
+            exist = True
+        else:
+            index += 1
+    
+    #if course exists remove it
+    if exist == True:
+        list.pop(index)
+
+'''
+Function Name: editCourse
+
+Purpose: edit an existing courses attributes eg. course name, hours, lab requirement
+
+Parameters: 
+        list: Which program(abbrev.) does course belong to (eg. PCOM(string)) 
+        valid lists: PCOM_TERM_1, PCOM_TERM_2, PCOM_TERM_3, BCOM_TERM_1, BCOM_TERM_2,
+                     BCOM_TERM_3, FS_TERM_1, FS_TERM_2, FS_TERM_3, DXDI_TERM_1, DXDI_TERM_2
+                     DXDI_TERM_3, BK_TERM_1, BK_TERM_2, BK_TERM_3, GL_TERM_1, GL_TERM_2, 
+                     GL_TERM_3, BA_TERM_1, BA_TERM_2, BA_TERM_3, PM_TERM_1, PM_TERM_2
+                     PM_TERM_3.
+
+        oldCourse: What course do you want to0 change (Eg. pcom_0199(String))
+
+        newCourse: New name of the course(Eg. cmsk_0199(string))
+      
+        hours: How many hours will it take to complete the course (Eg. 34 (Int))
+        
+        lab: is there a lab component for the lab (Eg. yes or no (string)(will be 
+             converted into a boolean))
+
+Example:
+        editCourse(PCOM_TERM_3, "CMSK_0101", "PCOM_0199", 34, no)
+
+        Result:
+            CMSK_0101 (course name will be changed to PCOM_0199)
+            if course exist it will change the attributes like so given above example:
+            course name will change to PCOM_0199
+            hours will change to 34
+            lab requirement will change to False
+            
+            before function call:
+                PCOM_TERM_3 = [..., ..., CMSK_0101]
+            after function call:
+                PCOM_TERM_3 = [..., ..., PCOM_0199]
+
+'''
+def editCourse(list, oldCourse, newCourse, hours, lab):
+    #initialize variables
+    oldCourse = oldCourse.upper()
+    newCourse = newCourse.upper()
+    hours = int(hours)
+    lab = lab.upper()
+    exist = False
+    i = 0
+    index = 0
+
+    #turn lab argument into a boolean
+    if lab == "YES":
+        lab = True
+    else:
+        lab = False
+    
+    #check to see whether old course exists
+    for i in range(len(list)):
+        if list[i].name == oldCourse:
+            exist = True
+            index = i
+    
+    #If old course exists edit the attributes
+    if exist == True:
+        list[index].name = newCourse
+        list[index].hours = hours
+        list[index].requires_lab = lab
+
+
+    
+#Test functions
+'''
 def main():
-    a = addCourse("pm","pcom_0131", 3, 18, "no")
-    print(a)
+    addCourse(PCOM_TERM_3,"pcom_0131", 18, "no")
+    print(PCOM_TERM_3)
+    print(PCOM_TERM_3[-1].name)
+    print(PCOM_TERM_3[-1].hours)
+    print(PCOM_TERM_3[-1].requires_lab)
+
+    editCourse(PCOM_TERM_3, "pcom_0131", "cmsk_0199", 28, "yes")
+
+    print(PCOM_TERM_3[-1].name)
+    print(PCOM_TERM_3[-1].hours)
+    print(PCOM_TERM_3[-1].requires_lab)
+    removeCourse(PCOM_TERM_3, "pcom_0131")
+    print(PCOM_TERM_3)
+    removeCourse(PCOM_TERM_3, "cmsk_0199")
+    print(PCOM_TERM_3)
+'''
 
 
-main()
+
+#main()
