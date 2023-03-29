523a6b3eddca61af801b3cba44b22bb0e84cad97
plysnh <62678881+plysnh@users.noreply.github.com>
2023-03-29 14:39:46 -0600

    edit help button, edit cohort

--- cohorts.py
+++ cohorts.py
@@ -41,7 +41,7 @@ term2core = {"PCOM": 150, "BCOM" : 50}
 term3core = {"PCOM": 315, "BCOM" : 35}
 dataPBCOM = [term1core, term2core, term3core]
 
-sizeClassroom = [24, 30, 36, 40]  #size of classrooms we have 
+sizeClassroom = [30, 24, 40, 36]  #size of classrooms we have 
 #mock class for cohort to test
 class Cohort:
     def __init__(self, program, term, cohort, size):
@@ -51,30 +51,94 @@ class Cohort:
         self.size = size
     
     def __str__(self):
+        #ex: PCOM0102 - term 1, cohort 2
         return f'{self.program}0{self.term}0{self.cohort}'
+    
 
-# Divide the Cohort 
+# Divide the Cohort for CORE Course PCOM + BCOM 
     '''
     ALgorithm: To populate all of the cohort the size of the room 
     ex: Given the sizes of available classrooms. 
     sizes = [24, 30, 36, 40]
 
     PCOM size of 150 students will consist of 
-            3  * 40-students room with full 40 students = 120
-            1  * 30-students room with full 30 students = 30
+            3  * 40-size room with full 40 students = 120
+            1  * 30-size room with full 30 students = 30
 
     In case of the size is 152 students, to prevent a cohort of size (2), 
     we will resize the cohort to 
-            3  * 40-students room with full 40 students = 120
-            1  * 30-students room with full 30 students = 30
+            3  * 40-size room with full 40 students = 120
+            1  * 36-size room with full 30 students = 32 
     '''
 # def getListOfClassroomSizes(size):
 #     print(set(size))
 #     return set(size)
 
-def coreCourseCohorts(size, dataPBCOM):
+def coreCourseCohorts(sizeClassroom, dataPBCOM):
+    
+    print(sorted(sizeClassroom,reverse = True)) # sort from biggest to smallest 
+    # biggestSize = max(sizeClassroom)
+    # smallestSize = min(sizeClassroom)
+    # print("Biggest size: " + biggestSize)
+    '''
+    The room with biggest size is reserved for BCOM since they have a large # of students
+    From 8am - 5pm (9hour), if class length is 2 hours, we can have 8/2 ~ 4 cohort of maximum size 
+    '''
+    for term in dataPBCOM:
+        for program in term:
+            numStudent = term[program]
+            #print(numStudent)
+            # cohortSize1 = 
+    sizeList = [315, 64, 11, 35]
+    coreCourseFor2Term(sizeList)
     return 
 
+'''
+At any moment in time, only 2 term are scheduled: 
+ex: 
+Fall: Only Term 1 + Term 3
+Winter: Only Term 2 + Term 1
+Spring: Only Term 3 + Term 2
+term1core = {"PCOM": 64, "BCOM" : 11}
+term2core = {"PCOM": 150, "BCOM" : 50}
+term3core = {"PCOM": 315, "BCOM" : 35}
+'''
+def coreCourseFor2Term(sizeList): 
+    
+    PCOMsum = sizeList[0] + sizeList[1]
+    BCOMsum = sizeList[2] + sizeList[3]
+    print("Total PCOM students: ", PCOMsum, "\nTotal BCOM students: ", BCOMsum)
+
+    sortedClassSize = sorted(sizeClassroom,reverse = True) # [40, 36, 30, 24]
+    sortedStudentSize = sorted(sizeList, reverse = True) 
+    
+    #classSize = [40, 36, 30, 24]
+    numClassroom = [1, 2,  3,  3] 
+    numCohorts = [4, 8, 12, 12]
+    for i in range(4): # [315, 64, 35, 11]
+        resultList = [0,0,0,0] 
+        origin = sortedStudentSize[i]
+        for n in range (4): # [40, 36, 30, 24]
+            s = origin // sortedClassSize[i]
+            if (s >= numCohorts[n] ): # if need more cohort
+                resultList[n] = numCohorts[n] 
+                origin = origin - resultList[n] * sortedClassSize[n]
+                #print("Origin: ", origin)
+                continue
+            if (s < numCohorts[n] & origin > 0):
+                resultList[n] = s
+                origin = origin - resultList[n] * sortedClassSize[n]
+                #print("Origin: ", origin)
+                continue
+
+            else: 
+                resultList[n] = s
+                break
+        
+        print("for", sortedStudentSize[i], ": ", resultList)
+
+            
+        
 
     
 def numCohorts(total):
@@ -123,13 +187,15 @@ def createCohortObj(program, term, cohort, size):
 
 
 def main():
-    for term in range (len(data)):
-        for program in data[term]:
-            cohorts = numCohorts(data[term][program])
-            listCohorts = studentsPerCohort(data[term][program], cohorts)
-            print(f'program {program} has {data[term][program]} students, divide into {cohorts} cohorts.')
-            for c in range (len(listCohorts)):
-                cohort = createCohortObj(program, term, c+1, listCohorts[c])
-                print(cohort)
+    coreCourseCohorts(sizeClassroom, dataPBCOM)
+
+    # for term in range (len(data)):
+    #     for program in data[term]:
+    #         cohorts = numCohorts(data[term][program])
+    #         listCohorts = studentsPerCohort(data[term][program], cohorts)
+    #         print(f'program {program} has {data[term][program]} students, divide into {cohorts} cohorts.')
+    #         for c in range (len(listCohorts)):
+    #             cohort = createCohortObj(program, term, c+1, listCohorts[c])
+    #             print(cohort)
 
 main()
